76a2c1f904f54ef80df86d99bf87c7ab062e5c54
FortJS <130043784+FortJS@users.noreply.github.com>
2023-10-12 13:00:38 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,76 @@
+<!-- 
+Author: AUGUST Quinn
+Notes: 
+- Do not copy without permission.
+- This website provides a free and efficient obfuscator for JavaScript, enhancing code security.
+-->
+
+let form = document.getElementById("form");
+
+let form2 = document.getElementById("form2");
+
+let javascriptCode = document.getElementById("javascriptCode");
+
+let lockDomains = document.getElementById("lockDomains");
+
+let output = document.getElementById("output");
+
+let obfuscateAnotherCode = document.getElementById("obfuscateAnotherCode");
+
+let copyBtn = document.getElementById("copyBtn");
+
+
+
+form.onsubmit = function (event) {
+
+  event.preventDefault();
+
+
+
+  let domainLockArray = lockDomains.value.split(",");
+
+
+
+  var obfuscationResult = JavaScriptObfuscator.obfuscate(javascriptCode.value, {
+
+    domainLock: domainLockArray,
+
+  });
+
+
+
+  output.value = obfuscationResult.getObfuscatedCode();
+
+
+
+  form2.classList.remove("d-none");
+
+  this.classList.add("d-none");
+
+
+
+  javascriptCode.value = "";
+
+  lockDomains.value = "";
+
+};
+
+
+
+obfuscateAnotherCode.onclick = function () {
+
+  form2.classList.add("d-none");
+
+  form.classList.remove("d-none");
+
+};
+
+
+
+copyBtn.onclick = function () {
+
+  navigator.clipboard.writeText(output.value);
+
+  alert("The output has been copied!");
+
+};
